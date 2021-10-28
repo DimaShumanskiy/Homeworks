4ce02386eb7814fdedb3b72699b33979ad690734
DimaShumanskiy <dimaihfn@gmail.com>
2021-10-28 15:06:23 +0300

    Updates

---
+++ precache-manifest.9c43b7f4c435da7ea9920d1625ba9c99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df4348bcee3cd3b06956a41f9e0dbe5c",
+    "url": "/DimaShumanskiy/Homeworks/index.html"
+  },
+  {
+    "revision": "914d07c5a067977e34d9",
+    "url": "/DimaShumanskiy/Homeworks/static/css/main.f2a86191.chunk.css"
+  },
+  {
+    "revision": "a5eec3b31ca7866aef3a",
+    "url": "/DimaShumanskiy/Homeworks/static/js/2.490fbe74.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DimaShumanskiy/Homeworks/static/js/2.490fbe74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "914d07c5a067977e34d9",
+    "url": "/DimaShumanskiy/Homeworks/static/js/main.89abee57.chunk.js"
+  },
+  {
+    "revision": "3b27541b2d950922904f",
+    "url": "/DimaShumanskiy/Homeworks/static/js/runtime-main.9e7f937e.js"
+  }
+]);
