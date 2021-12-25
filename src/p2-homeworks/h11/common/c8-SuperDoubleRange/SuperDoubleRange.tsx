049bbb56ef9be4ff14fba95cc2d4171a96d623d7
DimaShumanskiy <dimaihfn@gmail.com>
2021-12-25 15:59:44 +0300

    hw 11

--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
+import Slider from '@mui/material/Slider';
+import s from './../../HW11.module.css'
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
+    onChangeRange?: (value:number | number[]) => void
+    value: [number, number]
+    onChangeRangeO?: (value:number) => void
     // min, max, step, disable, ...
+}
+
+function valuetext(value: number) {
+    return `${value}`;
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
@@ -12,12 +19,24 @@ const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
         // min, max, step, disable, ...
     }
 ) => {
-    // сделать самому, можно подключать библиотеки
+    // сделать самому, можно подключать библиотеки  // React.ChangeEvent<{}>
+    const handleChange = (event: Event, value: (number | number[])) => {
+        onChangeRange && onChangeRange(value as number)
+    };
 
+    // const handleChange = (event: Event, newValue: number | number[]) => {
+    //     setValue(newValue as number[]);
+    // };
     return (
-        <>
-            DoubleRange
-        </>
+        <div className={s.box}>
+                <Slider
+                    value={value}
+                    onChange={handleChange}
+                    valueLabelDisplay="auto"
+                    aria-labelledby="range-slider"
+                    getAriaValueText={valuetext}
+                />
+        </div>
     )
 }
 
